e2fe824cf09c16491927795b5bf718d42a984e6b
Wouter Baert <wouter.baert@student.kuleuven.be>
2018-04-30 16:30:48 +0200

    ANN seems to be okay after basic testing.

--- code/ann.py
+++ code/ann.py
@@ -32,7 +32,7 @@ class ANN:
 		# Predicts the output for this input vector
 		# input_vector should be normalized
 		
-		return dot(self.output_weights, dot(self.hidden_weights, input_vector))
+		return ANN.ACTIVATION(dot(self.output_weights, ANN.VEC_ACTIVATION(dot(self.hidden_weights, input_vector))))
 	
 	@staticmethod
 	def frob_norm(a, b):
@@ -41,6 +41,8 @@ class ANN:
 		return sqrt(linalg.norm(a)**2 + linalg.norm(b)**2)
 	
 	def train(self, examples):
+		
+		#print("Training")
 		
 		# examples is a list of (input, output)-tuples
 		# input should be normalized
@@ -64,6 +66,9 @@ class ANN:
 				final_input = dot(self.output_weights, hidden_output)
 				predicted_output = ANN.ACTIVATION(final_input)
 				
+				#print("Output:", output)
+				#print("Predicted output:", predicted_output)
+				
 				# Used in calculations
 				prediction_error = output - predicted_output
 				output_derivative = ANN.D_ACTIVATION(final_input)
@@ -72,8 +77,12 @@ class ANN:
 				requested_hidden_change = prediction_error*output_derivative*self.output_weights
 				self.output_weights = self.output_weights + ANN.STEP_SIZE*prediction_error*hidden_output
 				
+				#print("After adjusting output weights:", ANN.ACTIVATION(dot(self.output_weights, hidden_output)))
+				
 				# Backpropagate requested hidden change to adjust hidden weights
 				self.hidden_weights = self.hidden_weights + ANN.STEP_SIZE*outer(requested_hidden_change*(ANN.VEC_D_ACTIVATION(hidden_input)), input_vector)
+				
+				#print("After adjusting hidden weights:", ANN.ACTIVATION(dot(self.output_weights, ANN.VEC_ACTIVATION(dot(self.hidden_weights, input_vector)))))
 				
 				# Check stop criteria
 				iteration += 1
@@ -90,10 +99,41 @@ class ANN:
 
 # TESTING
 
+def print_difference(ann1, ann2):
+	
+	# Prints the differences in weights in between two ANN's with identical topology
+	
+	hidden_weights1, output_weights1 = ann1.get_weights()
+	hidden_weights2, output_weights2 = ann2.get_weights()
+	hidden_diff = hidden_weights1 - hidden_weights2
+	output_diff = output_weights1 - output_weights2
+	
+	print(hidden_diff)
+	print(output_diff)
+	print("Frobenius norms:")
+	print("Hidden weights difference:", linalg.norm(hidden_diff))
+	print("Output weights difference:", linalg.norm(output_diff))
+	print("Both:", ANN.frob_norm(hidden_diff, output_diff))
+
+def RMSE(ann, examples):
+	
+	total = 0
+	for input_vector, output in examples:
+		total += (output - ann.predict(input_vector))**2
+	return sqrt(total)/len(examples)
+
+def generate_examples(amount, input_size, evaluate):
+	# evaluate is a function mapping an input vector onto a numerical value
+	examples = []
+	inputs = random.normal(0, 100, (amount, input_size))
+	for i in range(amount):
+		input_vector = inputs[i]
+		examples.append((input_vector, evaluate(input_vector)))
+	return examples
+
 def test():
 	
 	# Test the ANN by having it model another ANN with identical topology but unknown weights
-	
 	
 	input_size = 5
 	hidden_size = 3
@@ -101,35 +141,19 @@ def test():
 	model = ANN(input_size, hidden_size)
 	
 	# Generate training data
-	examples_amount = 1000
-	examples = []
-	inputs = random.normal(0, 100, (examples_amount, input_size))
-	for i in range(examples_amount):
-		input_vector = inputs[i]
-		examples.append((input_vector, real.predict(input_vector)))
-	
-	def print_difference(ann1, ann2):
-		
-		# Prints the differences in weights in between two ANN's with identical topology
-		
-		hidden_weights1, output_weights1 = ann1.get_weights()
-		hidden_weights2, output_weights2 = ann2.get_weights()
-		hidden_diff = hidden_weights1 - hidden_weights2
-		output_diff = output_weights1 - output_weights2
-		
-		print(hidden_diff)
-		print(output_diff)
-		print("Frobenius norms:")
-		print("Hidden weights difference:", linalg.norm(hidden_diff))
-		print("Output weights difference:", linalg.norm(output_diff))
-		print("Both:", ANN.frob_norm(hidden_diff, output_diff))
+	training_data = generate_examples(100000, input_size, real.predict)
+	validation_data = generate_examples(100000, input_size, real.predict)
 	
 	# Print initial difference, train, then print new difference
 	print("Initial difference:")
 	print_difference(real, model)
-	model.train(examples)
+	print("Initial RMSE (on training data):", RMSE(model, training_data))
+	print("Initial RMSE (on validation data):", RMSE(model, validation_data))
+	model.train(training_data)
 	print("After training:")
 	print_difference(real, model)
+	print("After training RMSE (on training data):", RMSE(model, training_data))
+	print("After training RMSE (on validation data):", RMSE(model, validation_data))
 
 if __name__ == "__main__":
 	test()
